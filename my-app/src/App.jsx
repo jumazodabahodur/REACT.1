2e9bbd4dbcb38771af2d49cdecb7f29967f503ac
jumazodabahodur <jumazodabahodurr@gmail.com>
2026-03-11 17:55:03 +0500

    Main

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react"
+
 const App = () => {
+  const [str, setStr] = useState("")
+
   return (
     <div>
-      
+      <input  type="text" value={str} onChange={(e) => setStr(e.target.value)}  className="border border-black p-2 outline-none" 
+      />
+      <h1> {str}</h1>
     </div>
   )
 }
