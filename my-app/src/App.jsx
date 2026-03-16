dc967735a2a94ecc6975fc66ae529a170312fb1a
jumazodabahodur <jumazodabahodurr@gmail.com>
2026-03-16 16:35:59 +0500

    34567

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,115 +1,47 @@
-import React,{useState} from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
-const Api2 = () =>{
+const App = () => {
+const Api = "http://localhost:3001/users"
+const [data,setData] = useState([])
 
-const [data,setData]=useState([
-      { id: 1, name: "Idibek", age: 17, status: false },
-        { id: 1, name: "Idibek", age: 17, status: false },
-        { id: 1, name: "Idibek", age: 17, status: false },
-])
-
-const [nameEdit,setNameEdit] =useState("")
-const [ageEdit,setAgeEdit] =useState("")
-const [search,setSearch]= useState("")
-const [idx,setIdx] = useState(null)
-const [statusEdit,setStatusEdit] = useState(false)
-
-
-const deleteUser = (id) =>{
-    setData(data.filter((e)=>e.id !==id))
+async function GetData() {
+  try {
+    const {data} = await axsios.get(Api)
+    setData(data)
+  } catch (error) {
+    console.error(error);
+    
+  }
 }
 
 
-const showEdit = (e)=>{
-    setNameEdit(e.name)
-    setAgeEdit(e.age)
-    setIdx(e.id)
-    setStatusEdit(e.status)
+async function DeleteData(id) {
+  try {
+    await axsios.delete(`${Api}/${id}`)
+  } catch (error) {
+  console.error(error);
+    
+  }
 }
 
-
-const handleSubmit = (event)=>{
-    event.preventDefault()
-
-    const obj ={
-        id: idx, name: nameEdit, age: ageEdit, status: statusEdit 
-    }
-
-    setData(data.map((e)=>e.id == idx?obj:e))
-
-    setIdx(null)
-    setNameEdit("")
-    setAgeEdit("")
-    setSearch("")
-
-
-
-    if (idx === null) {
-      const newUser = {
-        id: Date.now(), 
-        name: nameEdit,
-        age: ageEdit,
-        status: false
-      };
-      setData([...data, newUser]); 
-    } else {
-
-      const obj = { id: idx, name: nameEdit, age: ageEdit, status: statusEdit };
-      setData(data.map((item) => (item.id === idx ? obj : item)));
-    }
-
-
-    setIdx(null);
-    setNameEdit("");
-    setAgeEdit("");
-    setSearch("");
-
-}
-
-
-
-
+useEffect(()=>{
+  GetData()
+},[])
 
   return (
-    <div className='p-10'>
-
-        <form onSubmit={handleSubmit} className='p-10'>
-            <input className='border m-2 p-1' value={nameEdit} onChange={(e)=>setNameEdit(e.target.value)} type="text" />
-            <input className='border m-2 p-1' value={ageEdit} onChange={(e)=>setAgeEdit(e.target.value)} type="text" />
-            <button type='submit' className='bg-blue-700'>Save</button>
-        </form>
-        <input value={search} onChange={(e)=> setSearch(e.target.value)} type="search" />
+    <div>
+      {data.map((e)=>{
+        return(
+          <div>
+            <h1>{e.name}</h1>
+            <button onClick={()=>DeleteData(e.id)}></button>
+          </div>
+        )
+      })}
       
-      <table>
-<thead>
-    <tr>
-        <th>Name</th>
-        <th>Age</th>
-        <th>Actions</th>
-    </tr>
-</thead>
-
-<tbody>
-{data.filter((el)=>el.name.toLowerCase().includes(search.trim().toLowerCase()))
-.map((e)=>(
-<tr key={e.id}>
-<td>{e.name}</td>
-<td>{e.age}</td>
-<td>
-    <button onClick = {()=> showEdit(e)}>Edit</button>
-    <button onClick = {()=> deleteUser(e.id)}>Del</button>
-</td>
-
-</tr>
-
-))}
-</tbody>
-
-      </table>
-
-
     </div>
   )
 }
 
-export default Api2
+export default App
