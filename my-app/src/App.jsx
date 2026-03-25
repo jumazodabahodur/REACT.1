89449eeca9e4fef4cfd1687779608aa1c0b8a026
jumazodabahodur <jumazodabahodurr@gmail.com>
2026-03-25 11:51:15 +0500

    router

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from './Layout/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+      path:"home",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+    ]
+  },
+]);
 
 const App = () => {
-
-  return (
-    <div>
-      
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
 
---
+++ my-app/src/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link, Outlet } from "react-router-dom" 
+
+const Layout = () => {
+  return (
+    <div>
+      <nav style={{ display: 'flex', gap: '10px' }}>
+        <Link to="/home">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <hr />
+      <Outlet /> 
+    </div>
+  )
+}
+
+export default Layout
